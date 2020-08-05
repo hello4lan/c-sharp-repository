667aab7d4f36d16626651f48bbebe158e02ce52c
Alan Puga <puga16lara@outlook.com>
2020-08-05 17:17:34 -0500

    se agrego un proyecto de encryptacion

---
+++ EncryptApp/EncryptDecryptExample.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace EncryptApp
+{
+    class EncryptDecryptExample
+    {
+        private static RSAParameters publicKey;
+        private static RSAParameters privateKey;
+
+        static string CONTAINER_NAME = "My Container Name";
+
+        public enum KeySizes
+        {
+            SIZE_512 = 512,
+            SIZE_1024 = 1024,
+            SIZE_2024 = 2024,
+            SIZE_952 = 952,  
+            SIZE_1369 = 1369
+
+
+        };
+
+        static void Main(string[] args)
+        {
+            string message = "2343234678909876";
+            generateKeys();
+            byte[] encrypted = Encrypt(Encoding.UTF8.GetBytes(message));
+            byte[] decrypted = Decrypt(encrypted);
+
+            Console.WriteLine("Message: " + message);
+            Console.WriteLine("Encrypted : \n " + BitConverter.ToString(encrypted).Replace("-", " ") + "\n");
+            Console.WriteLine("Decripted : " + Encoding.UTF8.GetString(decrypted));
+
+            Console.ReadLine();
+
+        }
+
+        static void generateKeys()
+        {
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                publicKey = rsa.ExportParameters(false);
+                privateKey = rsa.ExportParameters(true);
+            }
+        }
+
+        static byte[] Encrypt(byte[] input)
+        {
+            byte[] encrypted;
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportParameters(publicKey);
+                encrypted = rsa.Encrypt(input, true);
+            }
+
+            return encrypted;
+        }
+
+        static byte[] Decrypt(byte[] input)
+        {
+            byte[] decrypted;
+
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportParameters(privateKey);
+                decrypted = rsa.Decrypt(input, true);
+
+            }
+            return decrypted;
+        }
+    }
+}
---
+++ EncryptApp/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EncryptApp
+{
+    class Program
+    {
+        /*
+        public static byte[] RSAEncrypt(byte[] DataToEncrypted ,RSAParameters RSAkeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] EncryptedData;
+                using(RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAkeyInfo);
+                    EncryptedData = RSA.Encrypt(DataToEncrypted, DoOAEPPadding);
+
+                    
+
+                }
+
+                return EncryptedData;
+            }catch(CryptographicException e)
+            {
+
+                Console.WriteLine(e.Message);
+                return null;
+            }
+
+            
+
+        }
+
+        public static byte[] RSADecrypt(byte[] DataToEncrypt , RSAParameters RSAkeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] DecryptedData;
+
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAkeyInfo);
+                    DecryptedData = RSA.Decrypt(DataToEncrypt, DoOAEPPadding);
+
+                    return DecryptedData;
+                }
+            }catch(CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                UnicodeEncoding ByteConverter = new UnicodeEncoding();
+                byte[] dataToEncrypted = ByteConverter.GetBytes("1234567812345678");
+                byte[] encryptedData;
+                byte[] decryptedData;
+
+                using(RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+
+                    encryptedData = RSAEncrypt(dataToEncrypted, RSA.ExportParameters(false), false);
+
+                    Console.WriteLine("ENCRYPTED DATA");
+                   
+
+                    decryptedData = RSADecrypt(encryptedData, RSA.ExportParameters(true), false);
+                    Console.WriteLine("DECRYPTED DATA");
+                    Console.WriteLine()
+                    for (int i = 0; i <= decryptedData.Length - 1; i++)
+                    {
+                        Console.WriteLine(decryptedData[i]);
+                    }
+
+
+                }
+
+
+                Console.WriteLine("Hello World!");
+
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+                    }
+    }
+        */
+
+    }
+}
